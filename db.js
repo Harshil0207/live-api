754719fe2c234446b503fd2bcdbf5fb0cc2a8fe8
Harshil0207 <harshilgohil2703@gmail.com>
2024-09-23 16:16:30 +0530

    first

--- db.js
+++ db.js
@@ -40,3 +40,47 @@ exports.getcustumerid = (req, res) => {
       }
     });
   };
+
+  exports.insert = (req, res) => {
+    const { custumer_name } = req.body;
+    var sql = "INSERT INTO tbl_custumer (custumer_name) VALUES (?)";
+    var values = [custumer_name];
+    con.query(sql, values, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error inserting custumer data");
+      } else {
+        res.send("custumer data added");
+      }
+    });
+  };
+  exports.update = (req, res) => {
+    const { id } = req.params;
+    const { custumer_id, custumer_name } = req.body;
+    var sql = "UPDATE tbl_custumer SET  custumer_name = ? WHERE custumer_id = ?";
+    var values = [custumer_name, custumer_id];
+  
+    con.query(sql, values, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error updating custumer data");
+      } else {
+        res.send("custumer data updated");
+      }
+    });
+  };
+  
+  exports.delete = (req, res) => {
+    const { custumer_id } = req.params;
+    const sql = "DELETE FROM tbl_custumer WHERE custumer_id = ?";
+  
+    con.query(sql, [custumer_id], (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error deleting custumer data");
+      } else {
+        res.send("custumer data deleted");
+      }
+    });
+  };
+  
--- route.js
+++ route.js
@@ -4,5 +4,7 @@ module.exports = (app) => {
 
    app.get("/getcustumer",db.getcustumer);
    app.get("/getcustumerid/:custumer_id", db.getcustumerid);
-   
+   app.post("/insert", db.insert);
+  app.put("/update/:custumer_id", db.update);
+  app.delete("/delete/:custumer_id", db.delete);
 }
